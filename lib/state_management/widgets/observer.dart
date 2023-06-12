e3922f7c58eced22592a768c53ba74fa05fdfe1c
Rahul Dev <therdm95@gmail.com>
2023-06-12 22:31:35 +0530

    Documentation for Observer

--- lib/state_management/widgets/observer.dart
+++ lib/state_management/widgets/observer.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:reactiv/reactiv.dart';
 
 part 'observer_n.dart';
+
 //
 // /// A widget that observes changes in a reactive variable and triggers a rebuild when the variable changes.
 // class Observer<T> extends StatelessWidget {
@@ -33,15 +34,25 @@ part 'observer_n.dart';
 //     );
 //   }
 // }
+//
 
+/// A widget that observes changes in a reactive variable and triggers a rebuild when the variable changes.
 class Observer<T> extends StatefulWidget {
+  /// Constructs an [Observer] widget.
+  ///
+  /// The [listenable] parameter is the reactive variable to listen to.
+  ///
+  /// The [update] parameter is a callback function that defines the widget to rebuild whenever the [listenable] variable changes.
   const Observer({
     Key? key,
     required this.listenable,
     required this.listener,
   }) : super(key: key);
 
+  /// The reactive variable to listen to for changes.
   final Reactive<T> listenable;
+
+  /// A callback function that defines the widget to rebuild whenever the [listenable] variable changes.
   final Widget Function(T data) listener;
 
   @override
