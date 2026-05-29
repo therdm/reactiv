3a3f8cb16b0be0552f55f0f5d860f4f791d1698e
Rahul Dev Mondal <87224071+therdm@users.noreply.github.com>
2026-05-30 03:23:40 +0530

    Revert "Add putOf dependency registration method"

--- test/dependency_test.dart
+++ test/dependency_test.dart
@@ -32,10 +32,6 @@ class TestController extends ReactiveController {
   }
 }
 
-abstract class BaseService {}
-
-class ConcreteService extends BaseService {}
-
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
@@ -51,14 +47,6 @@ void main() {
 
       final found = Dependency.find<TestController>();
       expect(identical(controller, found), isTrue);
-    });
-
-    test('should putOf and find dependency by base type', () {
-      final service = ConcreteService();
-      Dependency.putOf<BaseService, ConcreteService>(service);
-
-      final found = Dependency.find<BaseService>();
-      expect(identical(service, found), isTrue);
     });
 
     test('should support tagged dependencies', () {
